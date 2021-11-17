ab7883c320aeafcc3fe87ec79f507f98b3358ea8
B Randy King <brk@kount.com>
2021-11-17 15:48:34 -0700

    grpc gateway intro: start

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -3,36 +3,31 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+
 	"github.com/bxcodec/faker/v3"
 	todoapiv1 "github.com/itt-learning-groups/proto-contracts/todoapi/gen/go/v1"
-	"github.com/itt-learning-groups/todoapi-grpc/internal/interceptors"
 	"google.golang.org/grpc"
-	"log"
 )
 
 func main() {
 	serverPort := 50051
 	serverAddress := fmt.Sprintf("localhost:%d", serverPort)
-	
-	//conn, err := grpc.Dial(serverAddress, grpc.WithInsecure())
-	conn, err := grpc.Dial(
-		serverAddress,
-		grpc.WithInsecure(),
-		grpc.WithUnaryInterceptor(interceptors.PrintHeaders),
-	)
+
+	conn, err := grpc.Dial(serverAddress, grpc.WithInsecure())
 
 	defer conn.Close()
 	if err != nil {
 		log.Fatalf("failed to connect to server at %v: %v", serverAddress, err)
 	}
-	
+
 	client := todoapiv1.NewTodoapiServiceClient(conn)
-	
+
 	req := todoapiv1.CreateTodoRequest{
 		Name:        faker.Word(),
 		Description: faker.Sentence(),
 	}
-	
+
 	res, err := client.CreateTodo(context.Background(), &req)
 	if err != nil {
 		log.Printf("failed to create new Todo: %v", err)
--- cmd/server/services/todoapi.go
+++ cmd/server/services/todoapi.go
@@ -2,13 +2,14 @@ package services
 
 import (
 	"context"
+
 	"github.com/google/uuid"
 	todoapiv1 "github.com/itt-learning-groups/proto-contracts/todoapi/gen/go/v1"
 )
 
 // `go get github.com/itt-learning-groups/proto-contracts/todoapi/gen/go/v1` after syncing local with remote `proto-contracts` repo on Github
 
-type TodoapiServer struct {}
+type TodoapiServer struct{}
 
 func (tas *TodoapiServer) CreateTodo(ctx context.Context, req *todoapiv1.CreateTodoRequest) (*todoapiv1.CreateTodoResponse, error) {
 	id := uuid.New().String()
@@ -20,7 +21,3 @@ func (tas *TodoapiServer) CreateTodo(ctx context.Context, req *todoapiv1.CreateT
 		},
 	}, nil
 }
-
-//func (tas *TodoapiServer) CreateTodo(ctx context.Context, req *todoapiv1.CreateTodoRequest) (*todoapiv1.CreateTodoResponse, error) {
-//	return &todoapiv1.CreateTodoResponse{}, status.Errorf(codes.Internal, "generic internal error")
-//}
